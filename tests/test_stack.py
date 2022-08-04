146ebd703848b7efea28ce4ebc4bd2837b07787a
netandman <detyuchenkoma@gmail.com>
2022-08-04 15:01:52 +0300

    Add all files

---
+++ tests/test_stack.py
@@ -0,0 +1,16 @@
+def test_stack():
+    stack = []
+    stack.append('one')
+    stack.append('two')
+
+    assert stack.pop() == 'two'
+    assert stack.pop() == 'one'
+
+def test_emptiness():
+    stack = []
+    assert not stack
+    stack.append('one')
+    assert bool(stack)
+
+    stack.pop()
+    assert not stack
